ca7eaf672104bedf6556bf2c685c991a51ce9c67
Zatsando <zvracy13@gmail.com>
2020-12-18 21:12:48 +0200

    asdasd

---
+++ LearnJava/src/main/java/com/zatsando/learnjava/Microphone.java
@@ -0,0 +1,67 @@
+package com.zatsando.learnjava;
+
+public class Microphone {
+    //instance variables/ properties/ fields
+    private String name;
+    private String color;
+    private int model;
+
+    //constructors
+    public Microphone() {
+    }
+
+    public Microphone(String name, String color){
+        this.name=name;
+        this.color=color;
+    }
+
+    public Microphone(String name, String color, int model) {
+        this.name = name;
+        this.color = color;
+        this.model = model;
+    }
+
+
+    //getters setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    //Actions methods
+    public void turnOff() {
+        System.out.println(this.name + " Turn off");
+    }
+
+    public void turnOn() {
+        System.out.println(this.name + " Turn on");
+    }
+
+    public void setVolume() {
+        System.out.println(this.name + " Setting volume");
+    }
+
+    public String showDescription() {
+        return "Microphone name: " + this.name + " with color: " + this.color +
+                " and its model: " + this.model;
+    }
+}
--- LearnJava/src/main/java/com/zatsando/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/zatsando/learnjava/MyClass.java
@@ -4,57 +4,25 @@ public class MyClass {
 
     public static void main(String[] args) {
 
-//        System.out.println(showBio("zats", "i was born in athens", 32));
-       divideNumbers(3, 5);
-//        item += 1;
-//
-//        System.out.println("Result: " + item);
-//        addNumbers(12, 12);
-//        anotherMethod("Hello", 91);
-//        showName();
-    }
+        Microphone microphone = new Microphone("Blue Yeti", "Blue", 133543);
 
+        Microphone gHMic = new Microphone("GHZB", "red", 465);
+        microphone.setName("87");
+        microphone.setColor("blue");
+        microphone.setModel(134);
 
-//    public static void showName() {
-//        for (int i = 0; i < 20; i++) {
-//
-//            if (i % 3 == 0) {
-//                System.out.println(i + " is multiple of 3");
-//                System.out.println("from showname");
-//            }
-//        }
-//    }
+        System.out.println("new mic "+microphone.getModel());
 
-//    public static void addNumbers(int a, int b) {
-//        System.out.println("sum is: " + (a + b));
-//    }
-//
-//    public static void anotherMethod(String something, int age) {
-//        if (age > 90) {
-//            System.out.println("i am still young");
-//        }
-//        System.out.println("an other method " + something + " " + age);
-//    }
+        System.out.println(microphone.showDescription());
 
-    public static void divideNumberss(double first, int second) {
-        System.out.println("Result: " + first / second);
-    }
+        Microphone newMic = new Microphone("new mic", "green", 123);
 
-    public static void multiplyNumbers(int first, int second) {
-        System.out.println("Result:" + first * second);
-    }
+        Microphone otherMic = new Microphone();
+        otherMic.setColor("blue");
+        otherMic.setModel(123412);
+        otherMic.setName("ablabla");
 
-    public static void divideNumbers(double first, int second) {
+        Microphone grandMic = new Microphone("grand","yellow");
 
-        System.out.println("Result is "+first / second);
-    }
-
-    public static String showBio(String name, String bio, int age) {
-        String description = " My name is "
-                + name + bio+
-                " and I am "
-                + age +
-                " old.";
-        return description;
     }
 }
