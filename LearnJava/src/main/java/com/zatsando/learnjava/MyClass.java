4a744cdfc5df7fe5040d0016c82456b6006c28a7
Zatsando <zvracy13@gmail.com>
2020-12-18 17:57:19 +0200

    Initial commit

---
+++ LearnJava/src/main/java/com/zatsando/learnjava/MyClass.java
@@ -0,0 +1,17 @@
+package com.zatsando.learnjava;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        double a = 13;
+        double b = 11;
+        boolean isAged = true;
+
+        if (!isAged){
+            System.out.println("Indeed!");
+
+        }else {
+            System.out.println("not true");
+        }
+    }
+}
